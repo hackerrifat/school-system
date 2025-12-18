2644ffe2ac60e9c9c1591848aee4282cf7e251b2
Rifat Alam <alamrifat14@gmail.com>
2025-12-19 01:12:06 +0530

    fix login url by adding django auth urls

--- school/urls.py
+++ school/urls.py
@@ -4,7 +4,13 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     path('', lambda request: redirect('/accounts/login/')),
+
     path('admin/', admin.site.urls),
+
+    # 🔴 THIS LINE IS VERY IMPORTANT
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # your app urls
     path('accounts/', include('accounts.urls')),
     path('attendance/', include('attendance.urls')),
     path('fees/', include('fees.urls')),
