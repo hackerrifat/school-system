5f68e4ccc3d02ecffacb81dc24a8456f5fffe408
Rifat Alam <alamrifat14@gmail.com>
2025-12-19 01:03:29 +0530

    redirect root url to login

--- school/urls.py
+++ school/urls.py
@@ -9,16 +9,3 @@ urlpatterns = [
     path('attendance/', include('attendance.urls')),
     path('fees/', include('fees.urls')),
 ]
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # Django auth (login, logout)
-    path('accounts/', include('django.contrib.auth.urls')),
-
-    # Our app urls
-    path('accounts/', include('accounts.urls')),
-
-    path('attendance/', include('attendance.urls')),
-
-    path('fees/', include('fees.urls')),
-]
