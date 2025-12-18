9f020785ad3d039f0fa241a5bb091a0798aa3824
Rifat Alam <alamrifat14@gmail.com>
2025-12-19 00:59:07 +0530

    redirect root url to login

--- school/urls.py
+++ school/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
+urlpatterns = [
+    path('', lambda request: redirect('/accounts/login/')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('attendance/', include('attendance.urls')),
+    path('fees/', include('fees.urls')),
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
 
